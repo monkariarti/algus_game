c471f96f5154a1b2e541140f4f103cc02701f1ce
Константин <konstantine.shtikov@yandex.ru>
2017-06-09 17:03:10 +0500

    Пушка v0.5

--- src/elements/Gun.js
+++ src/elements/Gun.js
@@ -10,22 +10,40 @@ Gun.prototype.create = function() {
   this.Game.physics.enable(this.gun, Phaser.Physics.ARCADE);
   this.gun.body.immovable = true;
   this.gun.body.allowGravity = false;
-  this.gun.anchor.set(0.5);
+  this.gun.anchor.set(0, 0.5);
+  
+  this.circle = new Phaser.Circle(this.set.x+350, this.set.y+350, 700);
+  this.graphics = this.Game.add.graphics(0, 0);
+  this.graphics.lineStyle(1, 0x00ff00, 1);
+  this.graphics.drawCircle(this.circle.x, this.circle.y, this.circle.diameter);
 
   this.weapon = this.Game.add.weapon(150, 'weapon');
   this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
   this.weapon.bulletSpeed = 1000;
-  this.weapon.fireRate = 1000;
-  this.weapon.trackSprite(this.gun, 30, 0, true);
-  this.weapon.autofire = true;
+  this.weapon.fireRate = 2000;
+  this.weapon.trackSprite(this.gun, 60, 0, true);
+  this.weapon.bullets.forEach((bullet) => {
+    bullet.width = 20;
+    bullet.height = 40;
+  });
 }
 
 Gun.prototype.update = function() {
-  this.gun.rotation = this.Game.physics.arcade.angleBetween(this.gun, this.Game.Player.player);
-  
-  //this.weapon = this.Game.physics.arcade.moveToXY(this.Game.Guns[0].weapon, 800, 1000, 400);
-  //this.weapon.fireAtXY(this.Game.Player.player.body.velocity.x, this.Game.Player.player.body.velocity.y);
+
+  if (check_overlap(this.Game.Player.player, this.graphics)) {
+    this.gun.rotation = this.Game.physics.arcade.angleBetween(this.gun, this.Game.Player.player);
+    this.weapon.fire();
+  }
+
+  this.Game.physics.arcade.collide(this.weapon.bullets, this.Game.Player.player, function(bullet, enemy){bullet.death(); enemy.kill();}); 
 }
 
 
 module.exports = Gun;
+
+function check_overlap(spriteA, spriteB) {
+  var boundsA = spriteA.getBounds();
+  var boundsB = spriteB.getBounds();
+
+  return Phaser.Rectangle.intersects(boundsA, boundsB);
+}
